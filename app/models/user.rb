24e6fa04cc0aca4d3578f0320bb4593860e07301
Charles Lee <unknownsanon@gmail.com>
2023-10-24 11:26:04 +0800

    Added username on register webpage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :username
+  has_many :discussions
 end

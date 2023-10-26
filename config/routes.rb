9c2748b02d9f5dc5978f388adde2f108e78afa8a
LampClark <unknownsanon@gmail.com>
2023-10-27 07:57:15 +0800

    User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
   resources :discussions do
     resources :posts, only: [:create]
   end
+
+  get 'profile', to: 'profiles#show'
 end
